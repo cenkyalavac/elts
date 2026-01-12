785b1dec97839d9d92cded00274ebfa74369f221
Cenk Yalavaç <cenkyalavac@gmail.com>
2026-01-12 17:11:26 +0000

    File changes

--- functions/gmailCallback.ts
+++ functions/gmailCallback.ts
@@ -12,7 +12,8 @@ Deno.serve(async (req) => {
 
         const clientId = Deno.env.get('GOOGLE_CLIENT_ID');
         const clientSecret = Deno.env.get('GOOGLE_CLIENT_SECRET');
-        const redirectUri = `${url.origin}/api/functions/gmailCallback`;
+        const appId = Deno.env.get('BASE44_APP_ID');
+        const redirectUri = `https://app.base44.com/api/apps/${appId}/functions/gmailCallback`;
 
         // Exchange code for tokens
         const tokenResponse = await fetch('https://oauth2.googleapis.com/token', {
